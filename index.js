32cfbdbcc6d1051455c5c2fb5daefb2f13f74309
Kethlyn <kethlynsk17@outlook.com>
2024-07-03 23:13:44 -0300

    editado

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const prompt = require("prompt-sync")({});
 
-let num1 = 0
-let num2 = 0
+console.log("**CALCULADORA**")
+let num1 = +prompt("Digite o primeiro número: ")
+let num2 = +prompt("Digite o segundo número: ")
 
-console.log("**CALCULADORA**\nEscolha o número para a operação:\n1. ADIÇÃO\n2. SUBTRAÇÃO\n3. MULTIPLICAÇÃO\n4. DIVISÃO\n5. PORCENTAGEM")
+console.log("Escolha o número para a operação:\n1. ADIÇÃO\n2. SUBTRAÇÃO\n3. MULTIPLICAÇÃO\n4. DIVISÃO\n5. PORCENTAGEM")
 let operacao = +prompt()
 
 if(operacao != 1 && operacao != 2 && operacao != 3 && operacao != 4 && operacao != 5 || operacao == isNaN){
     operacao = +prompt("Não entendi. Aperte ENTER e comece de novo.")
     process.exit()
-}else{
-    num1 = +prompt("Digite o primeiro número: ")
-    num2 = +prompt("Digite o segundo número: ")
 }
+
 let mAdicao = require("./1adicao");
 let mSubtracao = require("./2subtracao")
 let mMultiplicacao = require("./3multiplicacao")
